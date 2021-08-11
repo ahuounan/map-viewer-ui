15895fa5eeb62b4999120687eb642775bc174203
Alan Hu <on.alanhu@gmail.com>
2021-08-11 23:05:11 +0100

    fix: RootActions typing

--- src/store/types.d.ts
+++ src/store/types.d.ts
@@ -1,4 +1,5 @@
-import { FeatureActions } from './feature/actions';
+import { CombinedState, Reducer } from 'redux';
+
 import { rootReducer } from './root';
 
 import { configureStore } from './';
@@ -7,4 +8,9 @@ export type Dispatch = ReturnType<typeof configureStore>['dispatch'];
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-export type RootActions = FeatureActions;
+export type RootActions = typeof rootReducer extends Reducer<
+  CombinedState<RootState>,
+  infer A
+>
+  ? A
+  : never;
